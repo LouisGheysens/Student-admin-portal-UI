1739221cf4b695a022ee0cbc6021c7db023d2420
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-01-20 22:02:12 +0100

    Add sorting, add pagination, add values from api in html table

---
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from '../models/ui-models/student.model';
+import { StudentService } from './student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+  students: Student[] = [];
+  displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit(): void {
+    this.studentService.getStudents()
+    .subscribe(
+        (succesResponse) => {
+          this.students = succesResponse;
+          this.dataSource = new MatTableDataSource<Student>(this.students);
+
+          if(this.matPaginator) {
+            this.dataSource.paginator = this.matPaginator;
+          }
+
+          if(this.matSort) {
+            this.dataSource.sort = this.matSort;
+          }
+        },
+        (errorResponse) => {
+          console.log(errorResponse);
+        }
+    );
+  }
+
+}
