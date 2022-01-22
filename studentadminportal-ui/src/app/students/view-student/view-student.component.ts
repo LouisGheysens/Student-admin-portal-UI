bd0375693ae2792b0a3c509f04ab73e44316cc9b
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-01-23 00:37:14 +0100

    Deleting student successfully

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationDurations } from '@angular/material/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
@@ -38,7 +39,8 @@ export class ViewStudentComponent implements OnInit {
   constructor(private readonly studentService: StudentService,
     private readonly route: ActivatedRoute,
     private readonly genderService: GenderService,
-    private snackbar: MatSnackBar) { }
+    private snackbar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -76,6 +78,24 @@ export class ViewStudentComponent implements OnInit {
 
       }
     );
+  }
+
+  onDelete(): void {
+    this.studentService.deleteStudent(this.student.id)
+    .subscribe(
+      (successResponse) => {
+        this.snackbar.open('Student deleted succesfully', undefined, {
+          duration: 2000
+        });
+
+        setTimeout(() => {
+          this.router.navigateByUrl('students');
+        }, 2000);
+      },
+      (errorResponse) =>{
+
+      }
+    )
   }
 
 }
